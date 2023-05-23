0fb4b1af284b5803daf57bbc94a558e83a1bac4e
Glenn Chiang <glennchiang24@gmail.com>
2023-05-23 08:04:35 +0800

    Fixed accuracy of timer Problem statement will now be hidden when displaying drill results Once drill ends, start button will immediately become active again, so the user will not have to quit and start Properly resets timer and problem number when drill restarts

--- main.js
+++ main.js
@@ -12,18 +12,20 @@ const timerDisplay = document.getElementById('timer');
 
 let timer; // Interval
 
+const problemContainer = document.querySelector('.problem-container');
+
 const num1Elem = document.getElementById('num1');
 const num2Elem = document.getElementById('num2');
 const operatorElem = document.getElementById('operator');
 const answerField = document.getElementById('answer');
 
-const results = document.querySelector('.results');
+const resultsElem = document.querySelector('.results');
 
 // Default settings
 let mode = 'min-time';
 let difficulty = 'medium';
 let timeLimit = 30;
-let numProblems = 20;
+let numProblems = 2;
 
 let drillIsActive = false;
 enableSettings();
@@ -31,12 +33,22 @@ enableSettings();
 
 startButton.addEventListener('click', function() {
     toggleStart();
-    drillWindow.classList.toggle('active');
+    resultsElem.classList.remove('show');    
 
     if (!drillIsActive) {
     // Start drill
         drillIsActive = true;
+
+        if (!drillWindow.classList.contains('active')) {
+            drillWindow.classList.add('active');
+        }
+
+        if (problemContainer.classList.contains('hide')) {
+            problemContainer.classList.remove('hide');
+        }
+
         answerField.focus(); // Once drill starts, go to the input field so the user can start typing immediately
+        disableSettings(); // Prevent user from changing settings while a drill is running
 
         if (mode === 'min-time') {
             runMinTimeDrill(difficulty, numProblems);
@@ -44,17 +56,21 @@ startButton.addEventListener('click', function() {
         else {
             runMaxNumDrill(difficulty, timeLimit);
         }
-        
-        disableSettings(); // Prevent user from changing settings while a drill is running
-        
+                
     // Quit drill
     } else {
-        drillIsActive = false;
-        endDrill();
-        enableSettings();
-
+        resetDrill();
+        drillWindow.classList.remove('active');
     } 
 })
+
+
+function resetDrill() {
+    drillIsActive = false;
+    clearInterval(timer);
+    answerField.value = '';
+    enableSettings();       
+}
 
 
 // Toggle button between Start and Quit
@@ -68,7 +84,7 @@ function toggleStart() {
     }
 }
 
-
+// Settings are enabled as long as no drill is running
 function enableSettings() {
     for (let button of settingButtons) {
         button.addEventListener('click', toggleSetting);
@@ -116,11 +132,6 @@ function toggleButton(clickedButton, settingButtons) {
 }
 
 
-function endDrill() {
-    clearInterval(timer);
-    answerField.value = '';
-}
-
 function displayResults(mode, difficulty, setting, score) {
     if (mode === 'min-time') {
         document.querySelector('.results span.mode').innerHTML = 'Blitz';
@@ -137,9 +148,10 @@ function displayResults(mode, difficulty, setting, score) {
     }
     document.querySelector('.results span.difficulty').innerHTML = difficulty;
  
-    results.classList.add('show');
-    results.focus();
+    resultsElem.classList.add('show');
+    resultsElem.focus();
 }
+
 
 // Solve the given number of problems in the shortest possible time
 function runMinTimeDrill(difficulty, numProblems) {
@@ -161,20 +173,23 @@ function runMinTimeDrill(difficulty, numProblems) {
             // If user enters correct answer, progress to next problem
             if (answer === currentProblem.solution) {
                 // Once user has solved all problems i.e. completed the drill:
-                if (problemNumber > numProblems) {
-                    clearInterval(timer);
-                    displayResults('min-time', difficulty, numProblems, timeElapsed);
+                if (problemNumber >= numProblems) {
+                    resetDrill();
+                    toggleStart();
+
+                    problemContainer.classList.add('hide');
+                    
+                    resultsElem.classList.add('show');
+                    displayResults('min-time', difficulty, numProblems, timeElapsed - 1);
                     return;
                 }
 
+                problemNumber += 1;
                 currentProblem = generateProblem(difficulty);
                 renderProblem(currentProblem, problemNumber);
-                problemNumber += 1;
 
             // If user enters wrong answer, do not progress to next problem
-            } else {
-                
-            }
+            } 
 
             answerField.value = '' // Clear the input field regardless of whether the answer is correct or wrong
         }
@@ -229,6 +244,71 @@ function renderProblem(problem, problemNumber) {
 function generateProblem(difficulty) {
     let problem; 
 
+    const operators = ['+', '-', 'x', '/']
+
+    // Generate random number within range
+    function randint(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    function generateEasyProblem() {
+        const operator = operators[randint(0,2)]; // no division
+        let num1, num2;
+
+        if (operator === '+' || operator === '-') {
+            num1 = randint(2, 100);
+            num2 = randint(2, 100);
+        } else if (operator === 'x') {
+            num1 = randint(2, 12);
+            num2 = randint(2, 12);
+        }
+        
+        const problem = {
+            num1: Math.max(num1, num2), // Num1 will always be greater than num2,
+            num2: Math.min(num1, num2), // so the solution will always be positive
+            operator
+        };
+
+        return problem;
+    }
+
+    function generateMediumProblem() {
+        const operator = operators[randint(0,2)];
+        const num1 = randint(2, 100);
+        let num2;
+
+        if (operator === '+' || operator === '-') {
+            num2 = randint(2, 100);
+        } else {
+            num2 = randint(2, 12);
+        }
+        
+        const problem = { num1, num2, operator };
+        return problem;
+    }
+
+    function generateHardProblem() {
+        const operator = operators[randint(0,2)];
+        const num1 = randint(2, 100);
+        const num2 = randint(2, 100);
+        const problem = { num1, num2, operator };
+        return problem;
+    }
+
+    function computeSolution(problem) {
+        let solution;
+
+        if (problem.operator === '+') {
+            solution = problem.num1 + problem.num2;
+        } else if (problem.operator === '-') {
+            solution = problem.num1 - problem.num2;
+        } else {
+            solution = problem.num1 * problem.num2;
+        }
+
+        return solution;
+    }
+
     if (difficulty === 'easy') {
         problem = generateEasyProblem();
     } else if (difficulty === 'medium') {
@@ -241,70 +321,7 @@ function generateProblem(difficulty) {
     return problem;
 }
 
-const operators = ['+', '-', 'x', '/']
 
-// Generate random number within range
-function randint(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-function generateEasyProblem() {
-    const operator = operators[randint(0,2)]; // no division
-    let num1, num2;
-
-    if (operator === '+' || operator === '-') {
-        num1 = randint(2, 100);
-        num2 = randint(2, 100);
-    } else if (operator === 'x') {
-        num1 = randint(2, 12);
-        num2 = randint(2, 12);
-    }
-    
-    const problem = {
-        num1: Math.max(num1, num2), // Num1 will always be greater than num2,
-        num2: Math.min(num1, num2), // so the solution will always be positive
-        operator
-    };
-
-    return problem;
-}
-
-function generateMediumProblem() {
-    const operator = operators[randint(0,2)];
-    const num1 = randint(2, 100);
-    let num2;
-
-    if (operator === '+' || operator === '-') {
-        num2 = randint(2, 100);
-    } else {
-        num2 = randint(2, 12);
-    }
-    
-    const problem = { num1, num2, operator };
-    return problem;
-}
-
-function generateHardProblem() {
-    const operator = operators[randint(0,2)];
-    const num1 = randint(2, 100);
-    const num2 = randint(2, 100);
-    const problem = { num1, num2, operator };
-    return problem;
-}
-
-function computeSolution(problem) {
-    let solution;
-
-    if (problem.operator === '+') {
-        solution = problem.num1 + problem.num2;
-    } else if (problem.operator === '-') {
-        solution = problem.num1 - problem.num2;
-    } else {
-        solution = problem.num1 * problem.num2;
-    }
-
-    return solution;
-}
 
 
 
